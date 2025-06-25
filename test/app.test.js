222c459f8f4c2e6aaf51547a9804fa4c2e4c1bb6
Relativity6 <francisco.bravo@relativity6.com>
2025-06-24 19:00:51 -0600

    wip14

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return HTML', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('Al final todo estará bien y si no está bien es porque no hemos llegado al final');
+    expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito');
   });
 });
